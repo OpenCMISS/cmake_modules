61f2ecd7e253e87b432e2e575da4af73fd8dbcba
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-12-15 17:30:31 +1300

    Add quotation marks around PYTHON_PACKAGE_CURRENT_NAME.

--- Templates/bindingsinfo.in.py
+++ Templates/bindingsinfo.in.py
@@ -4,5 +4,5 @@
 info = { "dir": "@VIRTUALENV_COMPLETE_INSTALL_PREFIX_WO_CONFIG@${BTYPE}",
          "toolchain": "@TOOLCHAIN@", "compiler": "@COMPILER@", "buildtype": "${BTYPE}",
          "mpi": "@OPENCMISS_MPI@", "mpi_home": "@OPENCMISS_MPI_HOME@", "mpi_buildtype": "@OPENCMISS_MPI_BUILD_TYPE@",
-         "library_path": "@LIBRARY_PATH@", "library": @PYTHON_PACKAGE_CURRENT_NAME@, "python_ver": "@PYTHONLIBS_VERSION_STRING@", "activate": "${ACTIVATE_SCRIPT}", }
+         "library_path": "@LIBRARY_PATH@", "library": "@PYTHON_PACKAGE_CURRENT_NAME@", "python_ver": "@PYTHONLIBS_VERSION_STRING@", "activate": "${ACTIVATE_SCRIPT}", }
 
