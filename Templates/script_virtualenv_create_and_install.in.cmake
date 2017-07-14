0ec02c591b0934a36e724a1c5b7115eba8e92b58
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-07-14 12:29:35 +1200

    Make pip install command work with lines exceeding shebang character limits.

--- Templates/script_virtualenv_create_and_install.in.cmake
+++ Templates/script_virtualenv_create_and_install.in.cmake
@@ -4,5 +4,13 @@ if (NOT EXISTS "@ACTIVATE_SCRIPT@")
 endif ()
 
 file(TO_NATIVE_PATH "@PACKAGE_BINARY_DIR@" SETUP_PY_DIR)
-execute_process(COMMAND @VIRTUALENV_COMPLETE_INSTALL_PREFIX@/@VENV_BINDIR@/pip install --upgrade "${SETUP_PY_DIR}")
+execute_process(COMMAND @VIRTUALENV_COMPLETE_INSTALL_PREFIX@/@VENV_BINDIR@/python -m pip install --upgrade "${SETUP_PY_DIR}"
+    RESULT_VARIABLE _RESULT OUTPUT_VARIABLE _OUTPUT ERROR_VARIABLE _ERROR)
 
+if (NOT "${RESULT_VARIABLE}" STREQUAL "0")
+    message(STATUS "Error installing into virtualenv command returned: ${_RESULT}")
+    message(STATUS "with output:")
+    message(STATUS "${_OUTPUT}")
+    message(STATUS "and errors:")
+    message(STATUS "${_ERROR}")
+endif ()
