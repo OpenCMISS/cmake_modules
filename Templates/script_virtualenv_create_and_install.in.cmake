930e6320e396d463831eac73c1a9b484834f8cbe
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-08-29 14:07:48 +1200

    Correct variable name in determining successful execution of virtualenv installation.

--- Templates/script_virtualenv_create_and_install.in.cmake
+++ Templates/script_virtualenv_create_and_install.in.cmake
@@ -7,7 +7,7 @@ file(TO_NATIVE_PATH "@PACKAGE_BINARY_DIR@" SETUP_PY_DIR)
 execute_process(COMMAND @VIRTUALENV_COMPLETE_INSTALL_PREFIX@/@VENV_BINDIR@/python -m pip install --upgrade "${SETUP_PY_DIR}"
     RESULT_VARIABLE _RESULT OUTPUT_VARIABLE _OUTPUT ERROR_VARIABLE _ERROR)
 
-if (NOT "${RESULT_VARIABLE}" STREQUAL "0")
+if (NOT "${_RESULT}" STREQUAL "0")
     message(STATUS "Error installing into virtualenv command returned: ${_RESULT}")
     message(STATUS "with output:")
     message(STATUS "${_OUTPUT}")
