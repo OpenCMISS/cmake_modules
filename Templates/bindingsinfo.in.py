14f2aed800ebd444d19fd5d2a4faebd9cc0943d7
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-07-29 14:53:43 +1200

    Use the new Python version description variables from the FindPython module.

--- Templates/bindingsinfo.in.py
+++ Templates/bindingsinfo.in.py
@@ -4,5 +4,5 @@
 info = { "dir": "@VIRTUALENV_COMPLETE_INSTALL_PREFIX_WO_CONFIG@${BTYPE}",
          "toolchain": "@_ACTIVE_TOOLCHAIN@", "compiler": "@_ACTIVE_COMPILER@", "buildtype": "${BTYPE}",
          "mpi": "@_ACTIVE_MPI@", "mpi_home": "@OPENCMISS_MPI_HOME@", "mpi_buildtype": "@OPENCMISS_MPI_BUILD_TYPE@",
-         "library_path": "@LIBRARY_PATH@", "library": "@PYTHON_PACKAGE_CURRENT_NAME@", "python_ver": "@PYTHONLIBS_VERSION_STRING@", "activate": "${ACTIVATE_SCRIPT}", }
+         "library_path": "@LIBRARY_PATH@", "library": "@PYTHON_PACKAGE_CURRENT_NAME@", "python_ver": "@Python_VERSION@", "activate": "${ACTIVATE_SCRIPT}", }
 
--- Templates/script_virtualenv_create_and_install.in.cmake
+++ Templates/script_virtualenv_create_and_install.in.cmake
@@ -7,7 +7,9 @@ file(TO_NATIVE_PATH "@PACKAGE_BINARY_DIR@" SETUP_PY_DIR)
 execute_process(COMMAND @VIRTUALENV_COMPLETE_INSTALL_PREFIX@/@VENV_BINDIR@/python -m pip install --upgrade "${SETUP_PY_DIR}"
     RESULT_VARIABLE _RESULT OUTPUT_VARIABLE _OUTPUT ERROR_VARIABLE _ERROR)
 
-if (NOT "${_RESULT}" STREQUAL "0")
+if ("${_RESULT}" STREQUAL "0")
+    message(STATUS "Successfully installed python bindings to: @VIRTUALENV_COMPLETE_INSTALL_PREFIX@.")
+else ()
     message(STATUS "Error installing into virtualenv command returned: ${_RESULT}")
     message(STATUS "with output:")
     message(STATUS "${_OUTPUT}")
