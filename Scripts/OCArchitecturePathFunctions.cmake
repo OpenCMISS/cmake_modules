028bb1ea5159cbc06acd08e6e61956dcbb4c2cc4
sgenie68 <g.soudlenkov@auckland.ac.nz>
2018-10-15 01:42:06 +0000

    Changed instrumentation variable

--- Scripts/OCArchitecturePathFunctions.cmake
+++ Scripts/OCArchitecturePathFunctions.cmake
@@ -153,13 +153,13 @@ endfunction()
 function(getInstrumentationPartArchitecturePath VARNAME)
 
     # Instrumentation
-    if ("${OC_INSTRUMENTATION}" STREQUAL "scorep")
+    if ("${OPENCMISS_INSTRUMENTATION}" STREQUAL "scorep")
         set(INSTRUMENTATION_PART /scorep)
-    elseif ("${OC_INSTRUMENTATION}" STREQUAL "vtune")
+    elseif ("${OPENCMISS_INSTRUMENTATION}" STREQUAL "vtune")
         set(INSTRUMENTATION_PART /vtune)
-    elseif ("${OC_INSTRUMENTATION}" STREQUAL "gprof")
+    elseif ("${OPENCMISS_INSTRUMENTATION}" STREQUAL "gprof")
         set(INSTRUMENTATION_PART /gprof)
-    elseif ("${OC_INSTRUMENTATION}" STREQUAL "none")
+    elseif ("${OPENCMISS_INSTRUMENTATION}" STREQUAL "none")
         set(INSTRUMENTATION_PART) # Do nothing
     else ()
         message(STATUS "Unknown instrumentation option. Ignoring.")
--- Scripts/OCInstrumentation.cmake
+++ Scripts/OCInstrumentation.cmake
@@ -1,10 +1,9 @@
 
-SET(OC_INSTRUMENTATION none)
 if (OPENCMISS_INSTRUMENTATION)
     STRING(TOLOWER "${OPENCMISS_INSTRUMENTATION}" OPENCMISS_INSTRUMENTATION)
-    SET(OC_INSTRUMENTATION ${OPENCMISS_INSTRUMENTATION})
+    SET(OPENCMISS_INSTRUMENTATION ${OPENCMISS_INSTRUMENTATION} CACHE STRING "Instrumentation to be added" FORCE)
     if (OPENCMISS_INSTRUMENTATION STREQUAL "scorep")
-        SET(OC_INSTRUMENTATION scorep)
+#        SET(OC_INSTRUMENTATION scorep)
 	# Reset compiler names to their scorep wrapper compiler name
 	if(DEFINED CMAKE_C_COMPILER)
 	  string(CONCAT CMAKE_C_COMPILER "scorep-" "${CMAKE_C_COMPILER}")
@@ -17,15 +16,17 @@ if (OPENCMISS_INSTRUMENTATION)
 	endif()
     elseif (OPENCMISS_INSTRUMENTATION STREQUAL "vtune")
 #        if(TOOLCHAIN STREQUAL "intel")
-          SET(OC_INSTRUMENTATION vtune)
+        # Do nothing
+#          SET(OC_INSTRUMENTATION vtune)
 #	else()
 #          message(WARNING "Can only use vtune instrumentation with an Intel toolchain. Proceeding with no instrumentation.")
 #        endif()
     elseif (OPENCMISS_INSTRUMENTATION STREQUAL "gprof")
-          SET(OC_INSTRUMENTATION gprof)
+        # Do nothing
+#          SET(OC_INSTRUMENTATION gprof)
     elseif (OPENCMISS_INSTRUMENTATION STREQUAL "none")
         # Do nothing
-
+#
     else()
         message(WARNING "Unknown instrumentation: ${OPENCMISS_INSTRUMENTATION}. Proceeding with no instrumentation.")
     endif()
